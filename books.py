ffb3f22b25f55723619344676556a187167e7c6f
Szymon Chodzidlo <schodevio@gmail.com>
2026-06-11 00:41:23 +0200

    Add basic CRUD for books

--- books.py
+++ books.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 BOOKS = [
-    {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
-    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
-    {"id": 3, "title": "1984", "author": "George Orwell"},
+    {
+        "id": 1,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "category": "Fiction"
+    },
+    {
+        "id": 2,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "category": "Fiction"
+    },
+    {
+        "id": 3,
+        "title": "1984",
+        "author": "George Orwell",
+        "category": "Dystopian"
+    },
 ]
 
 
@@ -15,5 +30,52 @@ async def read_root():
 
 
 @app.get("/books")
-async def read_books():
-    return BOOKS
+async def read_books(author: str = None, category: str = None):
+    output = []
+
+    for book in BOOKS:
+        if author and book["author"] != author:
+            continue
+        if category and book["category"] != category:
+            continue
+        output.append(book)
+
+    return output
+
+
+@app.post("/books")
+async def create_book(book: dict = Body(...)):
+    new_id = max(book["id"] for book in BOOKS) + 1
+    book["id"] = new_id
+    BOOKS.append(book)
+
+    return book
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book["id"] == book_id:
+            return book
+
+    return {"error": "Book not found"}
+
+
+@app.put("/books/{book_id}")
+async def update_book(book_id: int, updated_book: dict = Body(...)):
+    for book in BOOKS:
+        if book["id"] == book_id:
+            book.update(updated_book)
+            return book
+
+    return {"error": "Book not found"}
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for book in BOOKS:
+        if book["id"] == book_id:
+            BOOKS.remove(book)
+            return {"message": "Book deleted"}
+
+    return {"error": "Book not found"}
